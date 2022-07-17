f2ea00c652e1f0814d55663173f30548e757aa42
wingio <wingio@users.noreply.github.com>
2022-07-17 18:03:53 -0400

    Allow logcat to autoreconnect

--- app/src/main/java/xyz/wingio/logra/utils/logcat/LogcatManager.kt
+++ app/src/main/java/xyz/wingio/logra/utils/logcat/LogcatManager.kt
@@ -1,6 +1,7 @@
 package xyz.wingio.logra.utils.logcat
 
 import xyz.wingio.logra.domain.logcat.LogcatEntry
+import java.io.BufferedReader
 import java.io.InputStream
 import kotlin.concurrent.thread
 
@@ -8,22 +9,26 @@ object LogcatManager {
 
     private const val command = "logcat"
 
-    private lateinit var currentStr: InputStream
+    private lateinit var reader: BufferedReader
+    private lateinit var proc: Process
 
     fun connect() {
-        currentStr = Runtime.getRuntime().exec(command).inputStream
+        proc = Runtime.getRuntime().exec(command)
+        reader = proc.inputStream.bufferedReader()
     }
 
     fun listen(callback: (LogcatEntry) -> Unit) {
         thread(start = true) {
-            val reader = currentStr.bufferedReader()
-            while (reader.readLine() != null && reader.readLine().isNotEmpty()) {
-                val line = reader.readLine()
-                line.split("\n").forEach {
-                    try {
-                        val ent = LogcatEntry.fromLine(it)
-                        ent?.let(callback)
-                    } catch (th: Throwable) {
+            while (true) {
+                if(!proc.isAlive) { connect(); continue }
+                if(reader.readLine() != null && reader.readLine().isNotEmpty()) {
+                    val line = reader.readLine()
+                    line.split("\n").forEach {
+                        try {
+                            val ent = LogcatEntry.fromLine(it)
+                            ent?.let(callback)
+                        } catch (th: Throwable) {
+                        }
                     }
                 }
             }
