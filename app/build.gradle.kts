9897a44e1ddbfe092d4d97ea4f7455005ae65cca
wingio <wingio@users.noreply.github.com>
2022-07-20 14:21:24 -0400

    Release ALPHA 1.20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "xyz.wingio.logra"
         minSdk = 21
         targetSdk = 32
-        versionCode = 1310
-        versionName = "ALPHA - 1.10"
+        versionCode = 1320
+        versionName = "ALPHA - 1.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
