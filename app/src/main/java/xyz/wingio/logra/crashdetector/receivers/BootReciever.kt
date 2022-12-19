f2e227aba9424dd7e6367a90142c0221e1487ed9
wingio <wingio@users.noreply.github.com>
2022-12-18 20:28:30 -0500

    Fix: Only start crash service on boot if enabled

--- app/src/main/java/xyz/wingio/logra/crashdetector/receivers/BootReciever.kt
+++ app/src/main/java/xyz/wingio/logra/crashdetector/receivers/BootReciever.kt
@@ -3,13 +3,18 @@ package xyz.wingio.logra.crashdetector.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import xyz.wingio.logra.domain.manager.PreferenceManager
 import xyz.wingio.logra.utils.initCrashService
 
-class BootReceiver : BroadcastReceiver() {
+class BootReceiver : BroadcastReceiver(), KoinComponent {
+
+    val prefs: PreferenceManager by inject()
 
     override fun onReceive(context: Context, intent: Intent?) {
         if (intent?.action != Intent.ACTION_BOOT_COMPLETED) return
-        context.initCrashService()
+        if(prefs.crashDetectorEnabled) context.initCrashService()
     }
 
 }
